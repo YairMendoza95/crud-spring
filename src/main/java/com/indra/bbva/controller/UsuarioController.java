0b989b24b400d8d24ae3ee0eb11df22caa25db4c
YairMendoza95 <joseyair.95@gmail.com>
2021-03-08 16:19:05 -0600

    Creando interfaz para los usuarios

---
+++ src/main/java/com/indra/bbva/controller/UsuarioController.java
@@ -0,0 +1,20 @@
+package com.indra.bbva.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.indra.bbva.service.UsuarioService;
+
+@Controller
+public class UsuarioController {
+	@Autowired
+	private UsuarioService usuarioService;
+
+	@GetMapping("/usuarios")
+	public String usuarios(Model model) {
+		model.addAttribute("usuarios", usuarioService.getAllUsers());
+		return "users/usuarios";
+	}
+}
