f9db7aed2bb10e4e3876d9e4dd9bf82f541da573
YairMendoza95 <joseyair.95@gmail.com>
2021-03-08 11:47:48 -0600

    Implementacion de CRUD para usuarios

--- src/main/java/com/indra/bbva/CrudApplication.java
+++ src/main/java/com/indra/bbva/CrudApplication.java
@@ -1,13 +1,26 @@
 package com.indra.bbva;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.indra.bbva.repository.RolRepository;
+import com.indra.bbva.repository.UsuarioRepository;
+
 @SpringBootApplication
-public class CrudApplication {
+public class CrudApplication implements CommandLineRunner {
+
+	@Autowired
+	UsuarioRepository usuarioRepository;
+	@Autowired
+	RolRepository rolRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+	}
 }
