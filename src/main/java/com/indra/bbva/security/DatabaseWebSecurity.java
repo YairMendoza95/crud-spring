670e7e9d419a0c7bc0c2f158c975f0255d559dcd
YairMendoza95 <joseyair.95@gmail.com>
2021-03-10 13:20:13 -0600

    Creacion de la clase MvcConfig para configuracion global

--- src/main/java/com/indra/bbva/security/DatabaseWebSecurity.java
+++ src/main/java/com/indra/bbva/security/DatabaseWebSecurity.java
@@ -3,13 +3,11 @@ package com.indra.bbva.security;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.indra.bbva.service.UsuarioService;
 
@@ -24,12 +22,6 @@ public class DatabaseWebSecurity extends WebSecurityConfigurerAdapter {
 	String queryAuth = "SELECT NOMBREUSUARIO, ROL FROM ROLES WHERE NOMBREUSUARIO =?";
 	String query = "SELECT NOMBREUSUARIO, CLAVE,  ACTIVO  FROM USUARIOS WHERE NOMBREUSUARIO=?";
 
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-		return bCryptPasswordEncoder;
-	}
-
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery(query)
@@ -38,6 +30,14 @@ public class DatabaseWebSecurity extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.logout();
+		http.authorizeRequests()
+				// Recursos estaticos sin autorizacion
+				.antMatchers("/static/**", "/imagenes/**").permitAll()
+				// Vistas publicas sin autorizacion/autenticacion
+				.antMatchers("/home/index", "/home/").permitAll()
+				// URL que requieran autorizacion/autenticacion
+				.anyRequest().authenticated()
+				// login sin autorizacion/autenticacion
+				.and().formLogin().permitAll();
 	}
 }
