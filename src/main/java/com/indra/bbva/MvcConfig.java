53c27a75165f2893f7e8e2ebcdb8991457047d38
YairMendoza95 <joseyair.95@gmail.com>
2021-03-10 17:13:35 -0600

    Implementacion de seguridad con thymeleaf

--- src/main/java/com/indra/bbva/MvcConfig.java
+++ src/main/java/com/indra/bbva/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.indra.bbva;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,5 +12,8 @@ public class MvcConfig implements WebMvcConfigurer {
 	 * bCryptPasswordEncoder = new BCryptPasswordEncoder(); return
 	 * bCryptPasswordEncoder;
 	 */
-
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("auth/login.html");
+	}
 }
